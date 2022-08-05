03c9e2523cd0cb5ce4f57620942d9a8bfc286c4f
yll <lili11.yang@tcl.com>
2022-08-05 18:29:48 +0800

    init:初始化项目

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,11 @@
-console.log('react项目框架搭建')
+import { createRoot } from 'react-dom/client'
+import * as React from 'react'
+import '@styles/global.css' // 引入全局公共样式文件
+
+const container = document.getElementById('app')
+const root = createRoot(container as Element)
+
+// 采用hash路由模式
+root.render(
+    <div className='title'>欢迎来到react!</div>
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') // 拆分css 以外链方式引入到header中
 
 module.exports = {
     mode: "development", // 默认开发环境
@@ -15,7 +16,39 @@ module.exports = {
         port: 9090,
         hot: true, // 本地开发热更新 代码更改后自动重新编译
     },
+    module: {
+        rules: [
+            {
+                test: /\.(tsx?|js)$/,
+                use: 'ts-loader',
+                include: /src/, 
+            },
+            {
+                test: /\.css$/,
+                exclude: [/node_modules/],
+                // css-loader对css文件进行合并处理等 
+                // style-loader用于处理的css文件以style标签的形式嵌入到html页面中
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+        ]
+    },
     plugins: [
-        
-    ]
+        // 将bundle.js自动挂载到index.html
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './public/index.html'), // 要挂载的模板文件
+            filename: 'index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css', // filename打包的是同步代码
+            chunkFilename: '[id].css', // 异步代码
+        }),
+    ],
+    // 配置模块如何解析 便于开发
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@styles': path.resolve(__dirname, './src/styles'),
+        },
+        extensions: ['.tsx', '.js', '.jsx'], // 自动解析的扩展 用户在使用时可以不用带扩展名eg: import file from @/to/file
+    },
 }
