874685ad7b6360e3d2a99f8e2909170c1946c61d
lili11.yang <lili11.yang@tcl.com>
2023-12-20 15:23:57 +0800

    feat:完善二级导航

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,16 +1,56 @@
 /*
  * @Author: Delight-y
  * @Date: 2022-08-26 10:36:57
- * @Description: 
- * @LastEditors: Delight-y
- * @LastEditTime: 2023-12-14 17:06:25
+ * @Description: 发现音乐
+ * @LastEditors: Delight_y
+ * @LastEditTime: 2023-12-20 15:14:39
  */
-import React from "react";
+import { Button } from 'antd'
+import React from 'react'
+import home from '@styles/home.module.scss'
+import { useState } from 'react'
 
 export default function Home() {
+    // 当前所在二级导航
+    const [currentType, setCurrentType] = useState(1)
+    // 定义btn列表数据
+    const btnList: Array<any> = [
+        { text: '推荐', value: 0 },
+        { text: '排行榜', value: 1 },
+        { text: '歌单', value: 2 },
+        { text: '主播电台', value: 3 },
+        { text: '歌手', value: 4 },
+        { text: '新碟上架', value: 5 },
+    ]
+    // 二级导航点击事件
+    const btnClick = val => {
+        // 获取当前点击的二级导航类型
+        setCurrentType(val)
+    }
     return (
         <div>
-            <h1>this is 发现音乐 page!</h1>
+            <div className={home.nav}>
+                {btnList.map(btn => {
+                    return (
+                        <Button
+                            type="text"
+                            key={btn.value}
+                            value={btn.value}
+                            className={home.btn}
+                            onClick={() => btnClick(btn.value)}
+                        >
+                            <span
+                                style={{
+                                    backgroundColor: currentType === btn.value ? '#9B0909' : '',
+                                }}
+                            >
+                                {btn.text}
+                            </span>
+                        </Button>
+                    )
+                })}
+            </div>
+            <h1>this is {btnList[currentType].text} page!</h1>
         </div>
     )
 }
