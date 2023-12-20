16d731b0f0083f0cfdad44ccac4f451e21cacd70
lili11.yang <lili11.yang@tcl.com>
2023-12-20 15:28:34 +0800

    feat:路由切换时，重置二级导航选中初始值

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,16 +3,17 @@
  * @Date: 2022-08-26 10:36:57
  * @Description: 发现音乐
  * @LastEditors: Delight_y
- * @LastEditTime: 2023-12-20 15:14:39
+ * @LastEditTime: 2023-12-20 15:27:36
  */
 import { Button } from 'antd'
-import React from 'react'
+import React, { useEffect } from 'react'
 import home from '@styles/home.module.scss'
 import { useState } from 'react'
+import { useLocation } from 'react-router-dom'
 
 export default function Home() {
     // 当前所在二级导航
-    const [currentType, setCurrentType] = useState(1)
+    const [currentType, setCurrentType] = useState(0)
     // 定义btn列表数据
     const btnList: Array<any> = [
         { text: '推荐', value: 0 },
@@ -22,6 +23,13 @@ export default function Home() {
         { text: '歌手', value: 4 },
         { text: '新碟上架', value: 5 },
     ]
+    // 获取到当前路由信息
+    const location = useLocation()
+     // 切换路由
+     useEffect(() => {
+        // 重置数据
+        setCurrentType(0)
+    }, [location.pathname])
     // 二级导航点击事件
     const btnClick = val => {
         // 获取当前点击的二级导航类型
